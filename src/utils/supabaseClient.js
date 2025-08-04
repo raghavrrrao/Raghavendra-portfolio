44d652451718da4d9d1d7cc2cfaabd016954ae2b
raghavrrrao <raoraghav28@gmail.com>
2025-08-04 14:38:06 +0530

    ping server properly

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 export function getSupabase() {
     return window.supabase.createClient(
-        "https://vffhgfmrygcmnueoechg.supabase.co",
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmZmhnZm1yeWdjbW51ZW9lY2hnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMjk3MTcsImV4cCI6MjA2NDkwNTcxN30.53jTnQQJXduzCijxrQtignNXl29jbLNdjcqJYFOs6zY"
+        process.env.REACT_APP_SUPABASE_URL,
+        process.env.REACT_APP_SUPABASE_ANON_KEY
     );
 }
